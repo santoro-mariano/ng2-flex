ab121ce520087239ec800f0abfb421fc0c742d81
Mariano Santoro <santoro_mariano@hotmail.com>
2018-01-26 11:23:02 -0600

    Made some modifications in demo application

--- src/demoApp/app/app.component.ts
+++ src/demoApp/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FlexService } from "../../lib/core/flex.service";
+import { DeviceSize } from "../../lib/core/deviceSize";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    private deviceSize = DeviceSize;
+
+    constructor(private service: FlexService) {
+    }
 }
